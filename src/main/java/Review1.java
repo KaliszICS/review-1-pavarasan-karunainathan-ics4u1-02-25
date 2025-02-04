c00f749c5076b24cf733eaba27e8c2f4cd4df54c
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-02-04 21:31:47 +0000

    Questions Complete

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,26 @@
-public class Review1 {
-
-	public static void q1() {
-		//Write question 1 code here
+public class Review1{
+	public static void q1(){
+		System.out.println("There once was a man from St. Ives.");	
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static void q2(){
+		int nine = 9;
+		int five = 5;
+		System.out.println(nine/five);
+		System.out.println(nine%five);
 	}
 
-	public static void q3() {
-		//Write question 3 code here
+	public static void q3(){
+		System.out.println("Hello\nHello again");
 	}
 
-	public static void q4() {
-		//Write question 4 code here
+	public static void q4(){
+		System.out.println(true);
 	}
 
-	public static void q5() {
-		//Write question 5 code here
+	public static void q5(){
+		String hello = "Hello";
+		String world = "World";
+		System.out.print(hello + " "); System.out.println(world);
 	}
-
 }
